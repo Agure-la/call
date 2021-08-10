e9443d9a1f8cee65932f4a20ff8fd285d168a4fc
Agure-la <lameckagure@gmail.com>
2021-08-10 10:14:53 +0300

    task

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int total = 0;
+        int x = 0,k = 2, n = 4;
+        int[] arr = {1, 3, 2, 4};
+
+        System.out.println(leastCost(n - 1, k, total, arr, 0));
+    }
+
+    public static int leastCost(int n, int x, int total, int[] arr, int i){
+        if (n <= 0){
+            return total;
+        }
+
+        total = ((x + 1) * arr[i]);
+
+        return leastCost(n-x, x + 1, total, arr, i + 1);
+    }
+}
